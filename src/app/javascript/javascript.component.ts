a09a20dc54284c5ca8c4786a49a83e7eb67dc7c3
unknown <wrightkuku10@gmail.com>
2023-02-23 18:16:56 +0300

    services

--- src/app/javascript/javascript.component.ts
+++ src/app/javascript/javascript.component.ts
@@ -4,13 +4,20 @@ import { EnrollService } from '../services/enrol.service';
 @Component({
   selector: 'app-javascript',
   templateUrl: './javascript.component.html',
-  styleUrls: ['./javascript.component.css']
+  styleUrls: ['./javascript.component.css'],
+  providers:[EnrollService]
 })
 export class JavascriptComponent {
   title = "Javascript"
+
+constructor(private enrollService:EnrollService){
+
+}
+
+
   OnEnroll(){
-    const enrollService = new EnrollService();
-    enrollService.OnEnrollClick(this.title)
+    // const enrollService = new EnrollService();
+    this.enrollService.OnEnrollClick(this.title)
 
 
   }
--- src/app/typescript/typescript.component.ts
+++ src/app/typescript/typescript.component.ts
@@ -4,15 +4,22 @@ import { EnrollService } from '../services/enrol.service';
 @Component({
   selector: 'app-typescript',
   templateUrl: './typescript.component.html',
-  styleUrls: ['./typescript.component.css']
+  styleUrls: ['./typescript.component.css'],
+  providers:[EnrollService]
 })
 export class TypescriptComponent {
 
-  typescript="typecript"
+  title="typecript"
+
+  constructor( private enrollservice:EnrollService){
+
+  }
 
   OnEnroll(){
-    const enrollService = new EnrollService();
-    enrollService.OnEnrollClick(this.typescript)
+    // creating an instance of class by our owm is a bad practice.
+    // angular can do it for us using dependency injection
+    // const enrollService = new EnrollService();
+this.enrollservice.OnEnrollClick(this.title)
   }
 
 
